8b862a07cd44bf6d2a77be9f4d8b2759224ae54b
Mark Arciaga <mark.arciaga@gmail.com>
2017-09-04 14:25:43 -0700

    Fix webpack config to set prod env correctly

--- webpack.config.js
+++ webpack.config.js
@@ -10,36 +10,44 @@ let libraryName = 'FreeformDJNames';
 let plugins = [], outputFile;
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            }
+        }),
+    );
+
+    outputFile = libraryName + '.min.js';
 } else {
-  outputFile = libraryName + '.js';
+    outputFile = libraryName + '.js';
 }
 
 const config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/lib',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      },
-    ]
-  },
-  resolve: {
-    modules: [path.resolve('./node_modules'), path.resolve('./src')],
-    extensions: ['.json', '.js']
-  },
-  plugins: plugins
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/lib',
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/
+            },
+        ]
+    },
+    resolve: {
+        modules: [path.resolve('./node_modules'), path.resolve('./src')],
+        extensions: ['.json', '.js']
+    },
+    plugins: plugins
 };
 
 module.exports = config;
